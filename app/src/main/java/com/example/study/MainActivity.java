c6302fe59fd33db024bf8429c2d8888ede3047f6
HD-cjy <ccc5142@naver.com>
2022-08-14 17:35:32 +0900

    back task:random latter print

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.study;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -11,12 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            TextView tv = findViewById(R.id.tv);
 
             Random random = new Random();
             String[] arr ={"가","나","다","라","마"};
+            tv.setText(arr[random.nextInt(5)]);
 
 
-            TextView tv = findViewById(R.id.tv);
-            tv.setText("HI");
     }
 }
