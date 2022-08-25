b7826e2e302eebf578ff1320111eeb2dc3c8f495
HD-cjy <ccc5142@naver.com>
2022-08-26 04:36:18 +0900

    Cooperation_task

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -1,34 +1,71 @@
 package com.example.study;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
+    private int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activity = this; //
-        btn = findViewById(R.id.next);
-        btn.setOnClickListener(new View.OnClickListener() {
+        EditText input_text= findViewById(R.id.input_text);
+        Button input_btn= findViewById(R.id.input_btn);
+        TextView box1= findViewById(R.id.box1);
+        TextView box2= findViewById(R.id.box2);
+        TextView box3= findViewById(R.id.box3);
+        TextView box4= findViewById(R.id.box4);
+        TextView box5= findViewById(R.id.box5);
+        TextView output_text = findViewById(R.id.output_text);
+        Random random = new Random();
+        String [] arr= new String[5];
+        input_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            Intent i = new Intent(activity,test02.class);
-                startActivity(i);
+
+                switch (count) {
+                    case 0:
+                        arr[count] = input_text.getText().toString();
+                        box1.setText(arr[count]);
+                        count++;
+                        input_text.setText(null);
+                        break;
+                    case 1:
+                        arr[count] = input_text.getText().toString();
+                        box2.setText(arr[count]);
+                        count++;
+                        input_text.setText(null);
+                        break;
+                    case 2:
+                        arr[count] = input_text.getText().toString();
+                        box3.setText(arr[count]);
+                        count++;
+                        input_text.setText(null);
+                        break;
+                    case 3:
+                        arr[count] = input_text.getText().toString();
+                        box4.setText(arr[count]);
+                        count++;
+                        input_text.setText(null);
+                        break;
+                    case 4:
+                        arr[count] = input_text.getText().toString();
+                        box5.setText(arr[count]);
+                        input_btn.setText("변환");
+                        count++;
+                        input_text.setText(null);
+                        break;
+                    case 5:
+                        output_text.setText(arr[random.nextInt(5)]);
+                        break;
+                }
             }
         });
-
     }
-    public MainActivity(){super();}
-    private MainActivity activity;
-//    public void login(View v){
-//    btn = findViewById(R.id.login);
-    /*     Intent i = new Intent(activity,test01.class);
-
-     */
-    }
+}
